452193a1d6f7957af5d763ac40052e150e4cd2ec
Alexis Abner <alexisabner@alexiss-air.lan>
2023-12-12 18:32:02 -0700

    installed, imported packages, set up server

--- index.js
+++ index.js
@@ -8,7 +8,30 @@ const express = require('express'),
     app.use(express.urlencoded({
         extended: true
     }));
-    
+
+    //Require Mongoose package, "models.js" and Mongoose Models
+    const mongoose = require('mongoose');
+    const Models = require('./models');
+
+    //Connect Mongoose to myFlixDB
+    mongoose.connect('mongodb://localhost:27017/filmDB', 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    });         
+
+    const Movies = Models.Movie;
+    const Users = Models.User;
+    const Actors = Models.Actor;
+
+    app.get('/', (req, res) => {
+        res.send('Welcome to the movie app!');
+    });
+
+    app.get('/movies', (req, res) => {
+    res.send('Movies you will see!');
+    });
+
     app.listen(8080, () => {
         console.log("The app is listening on port 8080.")
     })
