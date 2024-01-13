65cdb7180eae22ba51169e6d716f4bb68fc056cb
Alexis Abner <alexisabner@alexiss-air.lan>
2024-01-12 22:05:20 -0700

    Updated allowedOrigins

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.use(
 const cors = require('cors');
 let allowedOrigins = [
         'http://localhost:8080', 
-        'http://localhost:1234/',
+        'http://localhost:1234',
 ];
 
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
-      let message = "The CORS policy for this application does'nt allow access from origin " + origin;
+      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
       return callback(new Error(message), false);
     }
     return callback(null, true);
